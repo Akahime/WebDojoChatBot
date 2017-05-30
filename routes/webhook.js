7a287319ade9f1227dffd5b83815a95a08614425
Alexandre Rozier <alexandre.rozier@student.ecp.fr>
2017-05-30 20:55:01 +0200

    send  message back

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,14 +2,49 @@ var express = require('express');
 var router = express.Router();
 var chatService = require('../server/chatService')
 /* GET hello world page. */
-router.get('/', function(req, res, next) {
-    if(req.query['hub.mode'] === 'subscribe' && chatService.authenticate(req)) {
+router.get('/', function (req, res, next) {
+    if (req.query['hub.mode'] === 'subscribe' && chatService.authenticate(req)) {
         res.status(200).send(req.query['hub.challenge']);
     }
-    else{
+    else {
         res.sendStatus(403);
     }
 });
+
+router.post('/', function (req, res) {
+    var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(pageID,event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
+});
+
+function receivedMessage(pageID,event) {
+    // Putting a stub for now, we'll expand it in the following steps
+    chatService.sendTextMessage(pageID,event.message);
+}
 
 
 module.exports = router;
