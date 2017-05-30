3da77bcb3bb1828b297c3f5feaea40e616ddecc6
Sarah Gross <sarah.gross@student.ecp.fr>
2017-05-30 21:56:39 +0200

    Page weather

--- routes/weather.js
+++ routes/weather.js
@@ -1,10 +1,10 @@
+/**
+ * Created by manu on 5/30/17.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('weather');
 });
-
-
-module.exports = router;
--- routes/webhook.js
+++ routes/webhook.js
@@ -58,5 +58,4 @@ function receivedMessage(event) {
     }
 }
 
-
 module.exports = router;
