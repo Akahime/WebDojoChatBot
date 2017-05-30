55517a972649b62d219b28919a503a472674c9b5
Alexandre Rozier <alexandre.rozier@student.ecp.fr>
2017-05-30 20:20:53 +0200

    Send authenticate at /webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var chatService = require('../server/chatService')
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.send(chatService.authenticate(req));
 });
 
 
